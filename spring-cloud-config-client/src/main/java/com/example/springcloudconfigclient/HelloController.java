f945607078f37d43beb6b450d9129ee5cf883329
daichuanhao <daichuanhao@bamboocloud.cn>
2020-11-09 21:05:02 +0800

    spring-cloud-config配置中心refresh

--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/HelloController.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.springcloudconfigclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// RefreshScope使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
+@RefreshScope
 public class HelloController {
     @Value("${neo.hello}")
     private String hello;
