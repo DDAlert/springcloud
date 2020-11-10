34e4f595da3b835371506cb8b38ce669ecc72387
daichuanhao <daichuanhao@bamboocloud.cn>
2020-11-10 20:52:51 +0800

    spring-cloud-config配置中心高可用

--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -2,7 +2,10 @@ package com.example.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient激活对配置中心的支持
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudConfigClientApplication {
 
